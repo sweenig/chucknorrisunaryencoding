6efe11d120ea3960466034762ead1bade604fac3
Stuart Weenig <github@weenig.com>
2025-05-01 14:16:45 -0500

    working

---
+++ the_fastest.py
@@ -0,0 +1,8 @@
+def get_best_time(times):
+    seconds = []
+    for time in times:
+        h, m, s = time.split(":")
+        seconds.append(int(h) * 3600 + int(m) * 60 + int(s))
+    return times[seconds.index(min(seconds))]
+
+print(get_best_time([input() for i in range(int(input()))]))
